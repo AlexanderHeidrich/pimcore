284c44dbfde9e546357b3878e0488ac1641488f6
alexander.heidrich <alexander.heidrich@basilicom.de>
2021-01-28 16:24:46 +0100

    Added support for multiple localizedFields

--- project/src/AppBundle/Controller/DefaultController.php
+++ project/src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends FrontendController
